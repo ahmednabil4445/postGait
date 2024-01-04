76e7430f427829568d447a5e574dfcb6ee7a1b90
Ahmed Nabil <132344473+ahmednabil4445@users.noreply.github.com>
2024-01-04 13:56:08 +0200

    Update bill.api.js

--- src/moduels/bill/bill.api.js
+++ src/moduels/bill/bill.api.js
@@ -1,7 +1,8 @@
 const app = require('express').Router()
 const { protectedRoutes } = require('../auth/auth.service')
-const { createBills, getAllbills, getSpecificBill, deleteBill, updateBill } = require('./bill.service')
+const { createBills, getAllbills, getSpecificBill, deleteBill, updateBill, searchBills } = require('./bill.service')
 app.route('/').post(protectedRoutes, createBills).get(getAllbills)
+app.route('/searchBills').get(searchBills)
 app.route('/:id').get(getSpecificBill).delete(deleteBill).put(updateBill)
 
 module.exports = app

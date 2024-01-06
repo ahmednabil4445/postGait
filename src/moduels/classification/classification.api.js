76326c42fa0503b1e2c400c32cace83c6408ef72
Ahmed Nabil <132344473+ahmednabil4445@users.noreply.github.com>
2024-01-06 13:39:45 +0200

    Update classification.api.js

--- src/moduels/classification/classification.api.js
+++ src/moduels/classification/classification.api.js
@@ -1,7 +1,9 @@
 const app = require('express').Router()
 const { protectedRoutes } = require('../auth/auth.service')
-const { getAllClassifications, updateClassification, deleteClassification, createClassification, getSpecificClassification } = require('./classification.service')
+const { getAllClassifications, updateClassification, deleteClassification, createClassification, getSpecificClassification, searchClassifications } = require('./classification.service')
 app.route('/').post(protectedRoutes, createClassification).get(getAllClassifications)
+app.route('/searchClassifications').get(searchClassifications)
+
 app.route('/:id').get(getSpecificClassification).delete(deleteClassification).put(updateClassification)
 
 module.exports = app

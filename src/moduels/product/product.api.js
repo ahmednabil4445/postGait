40a469664beb5dbe448edb8bc532e88409ba9173
Ahmed Nabil <132344473+ahmednabil4445@users.noreply.github.com>
2024-01-06 13:42:44 +0200

    Update product.api.js

--- src/moduels/product/product.api.js
+++ src/moduels/product/product.api.js
@@ -2,8 +2,10 @@ const app = require('express').Router()
 const { uploadSingleImage } = require('../../middleware/fileUpload')
 
 const { protectedRoutes } = require('../auth/auth.service')
-const { createProduct, getAllProducts, updateProduct, getSpecificProduct, deleteProduct } = require('./product.service')
+const { createProduct, getAllProducts, updateProduct, getSpecificProduct, deleteProduct, searchProducts } = require('./product.service')
 app.route('/').post(protectedRoutes,uploadSingleImage('image' , 'product'), createProduct).get(getAllProducts)
+app.route('/searchProducts').get(searchProducts)
+
 app.route('/:id').get(getSpecificProduct).delete(deleteProduct).put(updateProduct)
 
 module.exports = app

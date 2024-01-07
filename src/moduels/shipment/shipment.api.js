5ec23cda05780f654dbe27a10db8ac6d946aaec7
Ahmed Nabil <132344473+ahmednabil4445@users.noreply.github.com>
2024-01-07 09:14:42 +0200

    Update shipment.api.js

--- src/moduels/shipment/shipment.api.js
+++ src/moduels/shipment/shipment.api.js
@@ -1,7 +1,8 @@
 const app = require('express').Router()
 const { protectedRoutes } = require('../auth/auth.service')
-const { createShipment, updateShipment, getAllShipments, deleteShipment, getSpecificShipment } = require('./shipment.service')
+const { createShipment, updateShipment, getAllShipments, deleteShipment, getSpecificShipment, searchShipment } = require('./shipment.service')
 app.route('/').post(protectedRoutes, createShipment).get(getAllShipments)
+app.route('/searchShipment').get(searchShipment)
 app.route('/:id').get(getSpecificShipment).delete(deleteShipment).put(updateShipment)
 
 module.exports = app
